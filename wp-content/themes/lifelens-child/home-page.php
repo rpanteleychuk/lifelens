ea1191dcd0f1f0813aa3bfcc5e2225c9e5d077db
rpanteleychuk <r.panteleychuk@gmail.com>
2019-04-10 20:32:53 +0300

    test acf

--- wp-content/themes/lifelens-child/home-page.php
+++ wp-content/themes/lifelens-child/home-page.php
@@ -8,6 +8,39 @@ Template Post Type: page
 <?php get_header(); ?>
 
 
+<?php
 
+if( have_rows('content') ):
+
+    while( have_rows('content') ) : the_row();
+
+        // get layout
+        $layout = get_row_layout();
+
+
+        // layout_1
+        if( $layout === 'text' ): ?>
+
+            <div class="layout-1">
+                <p><?php the_sub_field('colums'); ?></p>
+                <p><?php the_sub_field('text'); ?></p>
+                <p><?php the_sub_field('text_2'); ?></p>
+            </div>
+
+        <?php // layout_2
+        elseif( $layout === 'image' ): ?>
+
+            <div class="layout-2">
+
+                <p><?php the_sub_field('caption'); ?></p>
+            </div>
+
+        <?php endif;
+
+    endwhile;
+
+endif;
+
+?>
 
 <?php get_footer(); ?>

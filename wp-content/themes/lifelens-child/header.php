bc7a5f546b21e5f190befc0c014491a797abf718
rpanteleychuk <r.panteleychuk@gmail.com>
2019-04-13 10:55:08 +0300

    start section 1, add background, heading,text

--- wp-content/themes/lifelens-child/header.php
+++ wp-content/themes/lifelens-child/header.php
@@ -16,6 +16,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
--- wp-content/themes/lifelens-child/home-page.php
+++ wp-content/themes/lifelens-child/home-page.php
@@ -7,40 +7,57 @@ Template Post Type: page
 
 <?php get_header(); ?>
 
+<div class="main-container">
 
-<?php
+    <?php
 
-if( have_rows('home_page') ):
+    if (have_rows('home_page')):
 
-    while( have_rows('home_page') ) : the_row();
+        while (have_rows('home_page')) : the_row();
 
-        // get layout
-        $layout = get_row_layout();
+            // get layout
+            $layout = get_row_layout();
 
-        // layout_1
-        if( $layout === 'section1' ): ?>
+            // layout_1
+            if ($layout === 'section1'): ?>
 
-            <div class="section1">
-                <p><?php the_sub_field('heading'); ?></p>
-                <img src="<?php the_sub_field('image'); ?>">
-                <p><?php the_sub_field('main_text'); ?></p>
-            </div>
+                <div class="section1" style="background-image: url(<?php the_sub_field('image'); ?>);">
+                    <div class="page-container">
+                        <h3 class="font-heading-white"><?php the_sub_field('heading'); ?></h3>
+                        <p><?php the_sub_field('main_text'); ?></p>
+                        <?php
+                        $link = get_sub_field('link');
+                        if ($link):
+                            $link_url = $link['url'];
+                            $link_title = $link['title'];
+                            $link_target = $link['target'] ? $link['target'] : '_self';
+                            ?>
+                            <a class="button" href="<?php echo esc_url($link_url); ?>"
+                               target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                        <?php endif; ?>
+                    </div>
+                </div>
 
-        <?php // layout_2
-        elseif( $layout === 'section2' ): ?>
 
-            <div class="section2">
-                <p><?php the_sub_field('heading'); ?></p>
-                <img src="<?php the_sub_field('image'); ?>">
-                <p><?php the_sub_field('main_text'); ?></p>
-            </div>
 
-        <?php endif;
+            <?php // layout_2
+            elseif ($layout === 'section2'): ?>
 
-    endwhile;
+                <div class="section2">
+                    <p><?php the_sub_field('heading'); ?></p>
+                    <img src="<?php the_sub_field('image'); ?>">
+                    <p><?php the_sub_field('main_text'); ?></p>
+                </div>
 
-endif;
 
-?>
+            <?php endif;
+
+        endwhile;
+
+    endif;
+
+    ?>
+
+</div>
 
 <?php get_footer(); ?>

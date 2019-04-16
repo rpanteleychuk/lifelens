b8f2a1751f7440e36687bd93e365dc4928403a2d
rpanteleychuk <r.panteleychuk@gmail.com>
2019-04-16 21:28:45 +0300

    edit function.php chenge get_stylesheet_directory_uri()

--- wp-content/themes/lifelens-child/functions.php
+++ wp-content/themes/lifelens-child/functions.php
@@ -3,11 +3,10 @@
 
 function lifelens_child_styles () {
 
-    wp_enqueue_style('normalize-style', get_template_directory_uri() . '/node_modules/normalize.css/normalize.css');
+    wp_enqueue_style('normalize-style', get_stylesheet_directory_uri() . '/node_modules/normalize.css/normalize.css');
 
-    wp_enqueue_style('lifelens-style', get_template_directory_uri() . '/assets/css/main.min.css');
+    wp_enqueue_style('lifelens-style', get_stylesheet_directory_uri() . '/assets/css/main.min.css');
 
-    wp_enqueue_style('custom', get_stylesheet_directory_uri() . '/assets/css/main.min.css');
 }
 
 add_action( 'wp_enqueue_scripts', 'lifelens_child_styles');

3fcaafa0ad046adf96270190213f2bf4fd84343f
rpanteleychuk <r.panteleychuk@gmail.com>
2019-04-16 20:14:01 +0300

    section2 done, add page container

--- wp-content/themes/lifelens-child/home-page.php
+++ wp-content/themes/lifelens-child/home-page.php
@@ -21,9 +21,10 @@ Template Post Type: page
             // layout_1
             if ($layout === 'section1'): ?>
 
-                <div class="section1" style="background: linear-gradient(to top right, rgba(0,0,0, 0),rgba(0,0,0, 80)), url(<?php the_sub_field('image'); ?>) no-repeat center;  background-size: cover;">
+                <div class="section1"
+                     style="background: url(<?php the_sub_field('image'); ?>) no-repeat center;  background-size: cover;">
                     <div class="page-container">
-                        <h3 class="font-heading-white"><?php the_sub_field('heading'); ?></h3>
+                        <h3 class="font-heading text-white"><?php the_sub_field('heading'); ?></h3>
                         <p class="main-text-font"><?php the_sub_field('main_text'); ?></p>
                         <?php
                         $link = get_sub_field('link');
@@ -39,15 +40,38 @@ Template Post Type: page
                 </div>
 
 
-
             <?php // layout_2
             elseif ($layout === 'section2'): ?>
 
                 <div class="section2">
-                    <p><?php the_sub_field('heading'); ?></p>
-                    <img src="<?php the_sub_field('image'); ?>">
-                    <p><?php the_sub_field('main_text'); ?></p>
+                    <div class="page-container">
+                        <p class="section2-font text-white"><?php the_sub_field('white_text'); ?><span
+                                    class="text-light-green"><?php the_sub_field('green_text'); ?></span></p>
+                        <div>
+                            <?php
+                            $link = get_sub_field('link');
+                            if ($link):
+                                $link_url = $link['url'];
+                                $link_title = $link['title'];
+                                $link_target = $link['target'] ? $link['target'] : '_self';
+                                ?>
+                                <a class="button-blue" href="<?php echo esc_url($link_url); ?>"
+                                   target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+
                 </div>
+
+
+                <!--            --><?php //// layout_3
+//            elseif ($layout === 'section2'): ?>
+                <!---->
+                <!--                <div class="section2">-->
+                <!--                    <p>--><?php //the_sub_field('heading'); ?><!--</p>-->
+                <!--                    <img src="--><?php //the_sub_field('image'); ?><!--">-->
+                <!--                    <p>--><?php //the_sub_field('main_text'); ?><!--</p>-->
+                <!--                </div>-->
 
 
             <?php endif;

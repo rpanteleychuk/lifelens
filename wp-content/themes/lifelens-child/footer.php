62d5fd6ba645d3fb394b98fe39c9b3c1c302f9f9
rpanteleychuk <r.panteleychuk@gmail.com>
2019-04-23 19:33:05 +0300

    footer add style first second menu

--- wp-content/themes/lifelens-child/footer.php
+++ wp-content/themes/lifelens-child/footer.php
@@ -16,30 +16,24 @@
 	<footer id="colophon" class="site-footer">
         <div class="d-flex">
             <div class="footer-first-menu">
-                <p>LEARN</p>
+                <h6>LEARN</h6>
                 <div class="style-footer-first-menu">
                     <?php wp_nav_menu('menu=first_footer_menu'); ?>
                 </div>
             </div>
             <div class="footer-second-menu">
-                <p>CONNECT</p>
+                <h6>CONNECT</h6>
                 <div class="footer-second-menu">
                     <?php wp_nav_menu('menu=second_footer_menu'); ?>
                 </div>
             </div>
             <div class="contact-form">
-                <p>JOIN</p>
+                <h6>JOIN</h6>
                 <?php echo do_shortcode('[contact-form-7 id="404" title="footer-form"]'); ?>
             </div>
         </div>
 
-
-
-
-
-
-
-	</footer><!-- #colophon -->
+    </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>

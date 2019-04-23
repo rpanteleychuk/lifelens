a1163a60731cd834b507af606976f7e7c31ffaa7
rpanteleychuk <r.panteleychuk@gmail.com>
2019-04-23 19:15:00 +0300

    footer code structure change

--- wp-content/themes/lifelens-child/footer.php
+++ wp-content/themes/lifelens-child/footer.php
@@ -14,25 +14,31 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+        <div class="d-flex">
+            <div class="footer-first-menu">
+                <p>LEARN</p>
+                <div class="style-footer-first-menu">
+                    <?php wp_nav_menu('menu=first_footer_menu'); ?>
+                </div>
+            </div>
+            <div class="footer-second-menu">
+                <p>CONNECT</p>
+                <div class="footer-second-menu">
+                    <?php wp_nav_menu('menu=second_footer_menu'); ?>
+                </div>
+            </div>
+            <div class="contact-form">
+                <p>JOIN</p>
+                <?php echo do_shortcode('[contact-form-7 id="404" title="footer-form"]'); ?>
+            </div>
+        </div>
 
 
 
-        <?php echo do_shortcode('[contact-form-7 id="404" title="Untitled"]'); ?>
 
 
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lifelens' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'lifelens' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'lifelens' ), 'lifelens', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
